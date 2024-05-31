28827ca32ca98286009b697ad03f720939846409
JacopoLombardi <lombajacopo@gmail.com>
2024-05-31 16:13:01 +0200

    [add] created project vue, maked scss

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+
+
+
+
+<script>
+
+  export default {
+    data(){
+      return{
+
+      }
+    },
+
+
+    methods:{
+
+    },
+
+
+    mounted(){
+      this.getApi();
+    }
+  }
+</script>
+
+
+
+
+
+
+<template>
+  <div>
+
+
+
+  </div>
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
