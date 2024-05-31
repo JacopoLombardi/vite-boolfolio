365bc8c88acaa45fce47fb3355ca007caa2516d9
JacopoLombardi <lombajacopo@gmail.com>
2024-05-31 16:35:20 +0200

    [add] created storage, call Api from front-end

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 
-
-
-
 <script>
+import axios from 'axios'
+import{store} from './data/store.js'
 
   export default {
     data(){
       return{
-
+        
       }
     },
 
 
     methods:{
-
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(result =>{
+          console.log(result.data)
+          // this.projects = result.data;
+        })
+        .catch(error =>{
+          console.log(error.message)
+        })
+      }
     },
 
 
@@ -27,11 +35,10 @@
 
 
 
-
 <template>
-  <div>
+  <div class="container_">
 
-
+    <h1>My Portfolio</h1>
 
   </div>
 </template>
