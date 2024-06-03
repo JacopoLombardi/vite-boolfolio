b850c429f14f560075d98a641d07ea93ca39cc63
JacopoLombardi <lombajacopo@gmail.com>
2024-06-03 16:41:54 +0200

    [add] work in progress structure for type & technology

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -47,36 +47,67 @@
 
 <template>
    <div class="d-flex flex-column align-items-center mt-5">
-     <h1>Home</h1>
+      <h1>Home</h1>
  
  
-     <!-- loading -->
-     <div
-       v-if="(loading)"
-     >
-       <p class="fs-1 fw-semibold mt-5">Loading....</p>
-     </div>
+      <!-- loading -->
+      <div
+         v-if="(loading)"
+      >
+         <p class="fs-1 fw-semibold mt-5">Loading....</p>
+      </div>
      
- 
-     <!-- project -->
-     <div 
-       v-else
-       class="mt-5"
-     >
-       <ul class="list-unstyled">
-         <li class="fs-5">
- 
-           <ProjectCard
-             v-for="project in projects"
-             :key="project.id"
- 
-             :ProjectObj="project"
-           />
- 
-         </li>
-       </ul>
-     </div>
-     
+      <div 
+        v-else
+        class="d-flex"
+      >
+
+      <!-- project -->
+         <div 
+            class="mt-5 me-7"
+         >
+            <label class="fw-semibold fs-4 mb-2">Project</label>
+            <ul class="list-unstyled">
+               <li class="fs-5">
+      
+                  <ProjectCard
+                     v-for="project in projects"
+                     :key="project.id"
+         
+                     :ProjectObj="project"
+                  />
+      
+               </li>
+            </ul>
+         </div>
+
+         <div>
+            <!-- Type -->
+            <div class="badge_type text-center">
+               <label class="fw-semibold fs-4 mb-2">Type</label>
+               <div>
+                  <span class="badge text-bg-primary mx-2">aaa</span>
+                  <span class="badge text-bg-primary mx-2">aaa</span>
+                  <span class="badge text-bg-primary mx-2">aaa</span>
+                  <span class="badge text-bg-primary mx-2">aaa</span>
+                  <span class="badge text-bg-primary mx-2">aaa</span>
+               </div>
+            </div>
+
+            <!-- Technology -->
+            <div class="badge_type text-center">
+               <label class="fw-semibold fs-4 mb-2">Technology</label>
+               <div>
+                  <span class="badge text-bg-primary mx-2">bbbb</span>
+                  <span class="badge text-bg-primary mx-2">bbbb</span>
+                  <span class="badge text-bg-primary mx-2">bbbb</span>
+                  <span class="badge text-bg-primary mx-2">bbbb</span>
+               </div>
+            </div>
+
+         </div>
+
+      </div>
    </div>
  </template>
 
